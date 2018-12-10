9376d5ba2feb159ef92c3d706ee0d40ebb975e0a
palmettos <nyoung0@gmail.com>
2018-12-10 15:32:33 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    context: path.resolve(__dirname),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+    ]
+};
